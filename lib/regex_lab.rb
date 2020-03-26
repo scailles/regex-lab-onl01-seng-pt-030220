a4e1c5ec77e6cbea9eb6e9191beb35b8ce87caf4
Shaina C <scailles23@gmail.com>
2020-03-26 23:28:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,12 @@
 def starts_with_a_vowel?(word)
-if word.match?(/^[AEIOU]|^[aeiou]/)
-  return true
-else false
-end
+  if word.match?(/^[AEIOU]|^[aeiou]/)
+    return true
+  else false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  
 end
 
 def words_five_letters_long(text)
